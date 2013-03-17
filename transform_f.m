184231b288a4883737232eccc54bf61a67090c27
Samo Sela <samo.sela@gmail.com>
2013-03-17 22:22:22 +0100

    ...

--- transform_f.m
+++ transform_f.m
@@ -1,10 +1,12 @@
-function [ trans ] = transform_f( FEATURES )
+function [trans, y] = transform_f( FEATURES )
 %TRANSFORM_F Transform/reformat - unroll BoW.
-% [ trans ] = transform_f( FEATURES )
+% [trans, y] = transform_f( FEATURES )
+%   
 %   FEATURES - format: FEATURES.subj(s).act(a).rec(r).F 
 %                      F = k x n, where k can vary in size                              
-%   F_trans  - format: m x n, m is count_number_of_features( FEATURES )
-% 
+%   trans - format: m x n, m is count_number_of_features( FEATURES )
+%     y   - class label 
+%
 % Author: Samo Sela
 % e-mail: samo.sela-at-gmail.com
 
@@ -18,6 +20,7 @@ numRec  = size(FEATURES.subj(1).act(1).rec,2);
 count = count_number_of_features( FEATURES );
 
 trans = zeros(count,555);
+y = zeros(count, 1);
 
 idx = 1;
 for i=1:numSubj
@@ -29,6 +32,7 @@ for i=1:numSubj
 %                 idx = idx + 1;
 %             end
             trans(idx:idx+num-1,:) = FEATURES.subj(i).act(j).rec(k).F;
+            y(idx:idx+num-1,:) = j;
             idx = idx + num;
         end
     end
